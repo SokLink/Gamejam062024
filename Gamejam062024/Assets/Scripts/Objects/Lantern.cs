0333be1dead0eb67da0c22c3dd482c513749796b
SokLink <137996008+SokLink@users.noreply.github.com>
2024-06-30 15:48:46 +0300

    15:48 30.06.2024

--- Gamejam062024/Assets/Scripts/Objects/Lantern.cs
+++ Gamejam062024/Assets/Scripts/Objects/Lantern.cs
@@ -1,12 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Lantern : ActivatedObject
 {
     [SerializeField] private GameObject _light;
+    [SerializeField] private List<GameObject> _shadows = new List<GameObject>();
 
     private void FixedUpdate()
     {
-        if (IsActive) _light.SetActive(true);
-        else _light.SetActive(false);
+        if (IsActive)
+        {
+            _light.SetActive(true);
+
+            if (_shadows.Count > 0)
+            {
+                foreach (GameObject shadow in _shadows)
+                {
+                    shadow.SetActive(false);
+                }
+            }
+        }
     }
 }
